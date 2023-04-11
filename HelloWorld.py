c90997173da872d52a6c55a129ae5d5df323d44f
Augusto Szymberg <129625203+Augusto-Szymberg@users.noreply.github.com>
2023-04-11 11:19:28 -0300

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,17 @@
+#            _____  _____ _____ _____   _____      _       _            
+#     /\    / ____|/ ____|_   _|_   _| |  __ \    (_)     | |           
+#    /  \  | (___ | |      | |   | |   | |__) | __ _ _ __ | |_ ___ _ __ 
+#   / /\ \  \___ \| |      | |   | |   |  ___/ '__| | '_ \| __/ _ \ '__|
+#  / ____ \ ____) | |____ _| |_ _| |_  | |   | |  | | | | | ||  __/ |   
+# /_/    \_\_____/ \_____|_____|_____| |_|   |_|  |_|_| |_|\__\___|_|   
+# By A.S.
+
+# The following program is to print a desired text in a fun way.
+# It browses through an ASCII array until it get to a desired letter.
+# Then it locks it and continues until it has the desired output.
+# This demo prints out hello world, you can edit it by changing the toPrint.
+# It gets exponentialy slower the longer the toPrint is. 
+                                                                     
 from threading import Thread
 import time
 
